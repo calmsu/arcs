2425e309b388e32b697e97c5bc75f62db6357031
Nick Reynolds <ndreynolds@gmail.com>
2012-02-11 19:52:43 -0500

    Moves coffee-scripts to webroot/coffee/ (out of js/)

--- app/webroot/js/models/resource.js
+++ app/webroot/js/models/resource.js
@@ -1,14 +1,16 @@
-var __hasProp = Object.prototype.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
-
-arcs.models.Resource = (function(_super) {
-
-  __extends(Resource, _super);
-
+var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
+arcs.models.Resource = (function() {
+  __extends(Resource, Backbone.Model);
   function Resource() {
     Resource.__super__.constructor.apply(this, arguments);
   }
-
   Resource.prototype.defaults = {
     id: null,
     mime_type: "image/png",
@@ -16,14 +18,12 @@ arcs.models.Resource = (function(_super) {
     created: null,
     public: false
   };
-
   Resource.prototype.urlRoot = arcs.baseURL + 'resources';
-
   Resource.prototype.parse = function(response) {
-    if (response.modified = response.created) response.modified = null;
+    if (response.modified = response.created) {
+      response.modified = null;
+    }
     return response;
   };
-
   return Resource;
-
-})(Backbone.Model);
+})();
--- app/webroot/js/models/tag.js
+++ app/webroot/js/models/tag.js
@@ -1,25 +1,26 @@
-var __hasProp = Object.prototype.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
-
-arcs.models.Tag = (function(_super) {
-
-  __extends(Tag, _super);
-
+var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
+arcs.models.Tag = (function() {
+  __extends(Tag, Backbone.Model);
   function Tag() {
     Tag.__super__.constructor.apply(this, arguments);
   }
-
   Tag.prototype.urlRoot = arcs.baseURL + 'tags';
-
   Tag.prototype.validate = function(attrs) {
     var tags;
     if (arcs.tagView != null) {
       tags = arcs.tagView.collection.pluck('tag');
       tags.push(attrs.tag);
-      if (_.uniq(tags).length !== tags.length) return arcs.log('non unique');
+      if (_.uniq(tags).length !== tags.length) {
+        return arcs.log('non unique');
+      }
     }
   };
-
   return Tag;
-
-})(Backbone.Model);
+})();
