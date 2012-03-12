375a09e3d729301572184b88d39641f7793af885
Nick Reynolds <ndreynolds@gmail.com>
2012-03-12 16:42:50 -0400

    Adds some convenience methods to the cake models.

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -2,4 +2,14 @@
 class Tag extends AppModel {
     public $name = 'Tag';
     public $belongsTo = array('User', 'Resource');
+
+    public function fromString($string) {
+        $tags = array();
+        foreach(explode(',', $string) as $t) {
+            $t = trim($t);
+            if (strlen($t))
+                $tags[] = $t;
+        }
+        return $tags;
+    }
 }
