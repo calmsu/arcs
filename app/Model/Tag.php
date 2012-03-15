4551a0853f002d0df85318e39b21ffd3ea5c3b76
Nick Reynolds <ndreynolds@gmail.com>
2012-03-15 13:22:30 -0400

    Defines whitelist for the Tag model.

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -2,6 +2,7 @@
 class Tag extends AppModel {
     public $name = 'Tag';
     public $belongsTo = array('User', 'Resource');
+    public $whitelist = array('tag', 'resource_id');
 
     public function fromString($string) {
         $tags = array();
