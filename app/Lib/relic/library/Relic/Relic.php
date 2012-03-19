ce171f8723ed7eeb7689f1f263ec09411d4aae5c
Nick Reynolds <ndreynolds@gmail.com>
2012-03-19 15:05:21 -0400

    Don't check for relic deps unless using cli.

--- app/Lib/relic/library/Relic/Relic.php
+++ app/Lib/relic/library/Relic/Relic.php
@@ -25,26 +25,28 @@ $PHP_DEPS = array(
     'Imagick' => false
 );
 
-// Verify that each shell dependency is an executable in the user's path.
-foreach ($SHELL_DEPS as $dep => $avail) {
-    $path = getenv('PATH');
-    foreach (explode(':', $path) as $dir) {
-        if (is_executable($dir . DS . $dep)) {
-            $avail = true;
-            break;
+if (php_sapi_name() == 'cli') {
+    // Verify that each shell dependency is an executable in the user's path.
+    foreach ($SHELL_DEPS as $dep => $avail) {
+        $path = getenv('PATH');
+        foreach (explode(':', $path) as $dir) {
+            if (is_executable($dir . DS . $dep)) {
+                $avail = true;
+                break;
+            }
         }
+        if (!$avail)
+            trigger_error("$dep not found. Attemping to continue.\n");
+        $SHELL_DEPS[$dep] = $avail;
     }
-    if (!$avail)
-        trigger_error("$dep not found. Attemping to continue.\n");
-    $SHELL_DEPS[$dep] = $avail;
-}
 
-// Verify that each PHP dependency is an existing class or function.
-foreach ($PHP_DEPS as $dep => $avail) {
-    if (class_exists($dep) || function_exists($dep)) {
-        $PHP_DEPS[$dep] = true;
-    } else {
-        $PHP_DEPS[$dep] = false;
-        trigger_error("$dep not found. Attemping to continue.\n");
+    // Verify that each PHP dependency is an existing class or function.
+    foreach ($PHP_DEPS as $dep => $avail) {
+        if (class_exists($dep) || function_exists($dep)) {
+            $PHP_DEPS[$dep] = true;
+        } else {
+            $PHP_DEPS[$dep] = false;
+            trigger_error("$dep not found. Attemping to continue.\n");
+        }
     }
 }
