6246e3ef4d0cea9bc5cd7c9bc3e2503f3296c31b
Nick Reynolds <ndreynolds@gmail.com>
2012-03-23 15:16:20 -0400

    Adds whitelist to Hotspot model.

--- app/Model/Hotspot.php
+++ app/Model/Hotspot.php
@@ -2,4 +2,7 @@
 class Hotspot extends AppModel {
     public $name = 'Hotspot';
     public $belongsTo = array('Resource', 'User');
+    public $whitelist = array(
+        'resource_id', 'title', 'caption', 'link', 'x1', 'y1', 'x2', 'y2'
+    );
 }
